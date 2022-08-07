6698a3fc2d5b75614aaea513c5a178db80b4c15a
Pomidorkin <pbriuzgin@gmail.com>
2022-08-07 18:03:19 +0900

    Reverse logic added

--- Assets/SpawnEffect.cs
+++ Assets/SpawnEffect.cs
@@ -7,7 +7,7 @@ public class SpawnEffect : MonoBehaviour
     // First model
     [SerializeField] private Renderer _renderer;
     [SerializeField] private Material originalMaterial;
-    [SerializeField] private Material dissolveMaterial;
+    //[SerializeField] private Material dissolveMaterial;
     [SerializeField] private Material phaseMaterial;
 
 
@@ -15,10 +15,14 @@ public class SpawnEffect : MonoBehaviour
     [SerializeField] GameObject secondModel;
     [SerializeField] private Renderer _secondRenderer;
     [SerializeField] private Material secondModelOriginalMaterial;
-    [SerializeField] private Material secondModelPhaseMaterial;
+    [SerializeField] private Material secondModelPhaseMaterial; // Reversed shader material
 
 
     [SerializeField] private float fadeTime = 2f;
+
+    [SerializeField] bool TEST_BUTTON_DELETE_ME = false;
+
+    private bool fadeBackwards = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,10 +32,22 @@ public class SpawnEffect : MonoBehaviour
 
     private void DoFade(float start, float dest, float time)
     {
+        if (fadeBackwards)
+        {
+            _renderer.material = secondModelPhaseMaterial;
+            _secondRenderer.material = phaseMaterial;
+        }
+        else
+        {
+            _secondRenderer.material = secondModelPhaseMaterial;
+            _renderer.material = phaseMaterial;
+
+        }
         iTween.ValueTo(gameObject, iTween.Hash(
             "from", start, "to", dest, "time", time, "onupdatetarget", gameObject,
             "onupdate", "TweenOnUpdate", "oncomplete", "TweenOnComplete", "easetype", iTween.EaseType.easeInOutCubic
             ));
+        //fadeBackwards = true;
     }
 
     void TweenOnUpdate(float value)
@@ -42,7 +58,26 @@ public class SpawnEffect : MonoBehaviour
 
     void TweenOnComplete()
     {
-        _renderer.material = originalMaterial;
+        if (!fadeBackwards)
+        {
+            _renderer.material = originalMaterial;
+            //_secondRenderer.material = 
+        }
+        else
+        {
+            _secondRenderer.material = originalMaterial;
+        }
+        
         //_secondRenderer.material = originalMaterial;
+        fadeBackwards = !fadeBackwards;
+    }
+
+    private void Update()
+    {
+        if (TEST_BUTTON_DELETE_ME)
+        {
+            DoFade(0, 2, fadeTime);
+            TEST_BUTTON_DELETE_ME = false;
+        }
     }
 }
